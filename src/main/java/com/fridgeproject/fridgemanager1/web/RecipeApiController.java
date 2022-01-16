282dc579d6fe63f6cc006a8120f8d180e89c3f46
oddity11 <sen9563@naver.com>
2022-01-17 06:18:19 +0900

    recipe 기능 추가중

--- src/main/java/com/fridgeproject/fridgemanager1/web/RecipeApiController.java
+++ src/main/java/com/fridgeproject/fridgemanager1/web/RecipeApiController.java
@@ -10,8 +10,6 @@ import com.fridgeproject.fridgemanager1.service.RecipeService;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
